fbee47f1c0d1f1981e35fbf7060ea6ccbc4dc33a
cnitihc0@163.com <admin12345679>
2017-11-02 17:46:37 +0800

    添加spring cloud网关服务

---
+++ trans-service-zuul/src/main/java/com/spring/cloud/ZuulApplication.java
@@ -0,0 +1,17 @@
+package com.spring.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author mashaobo
+ * @create 2017-11-02 17:37
+ */
+@EnableZuulProxy
+@SpringCloudApplication
+public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+}
